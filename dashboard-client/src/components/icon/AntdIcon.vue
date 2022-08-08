672abcae10582caecaf7b0fa067baab2b9439e0c
Phuc Nguyen <phucnt.tech@gmail.com>
2022-08-08 19:58:14 +0700

    Remove icon

--- dashboard-client/src/components/icon/AntdIcon.vue
+++ dashboard-client/src/components/icon/AntdIcon.vue
@@ -1,31 +1,7 @@
 <template>
-    <component v-if="isLoaded" :is="icon" />
+    <div></div>
 </template>
 
 <script>
-import * as AntdIcons from '@ant-design/icons-vue/lib/icons'
 
-export default {
-    data() {
-        return {
-            isLoaded: false,
-            iconCollection: {}
-        }
-    },
-    props: {
-        name: {
-            type: String,
-            require: true
-        }
-    },
-    computed: {
-        icon() {
-            return this.iconCollection[this.name] ?? null;
-        }
-    },
-    mounted() {
-        this.isLoaded = true
-        this.iconCollection = AntdIcons;
-    }
-}
 </script>
