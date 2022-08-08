789eeeb9c41d40d14483ef29236af9c208bcf92e
daitech20 <90186867+daitech20@users.noreply.github.com>
2022-08-08 19:42:25 +0700

    Update vite config

--- dashboard-client/vite.config.js
+++ dashboard-client/vite.config.js
@@ -9,23 +9,6 @@ export default ({mode}) => {
 
   return defineConfig({
     plugins: [vue()],
-    base: 'http://localhost:8088',
-    build: {
-      outDir: './../staticfiles/dashboard/dist',
-      rollupOptions: {
-        output: {
-            // Prevent vendor.js being created
-            manualChunks: undefined,
-            entryFileNames: "assets/[name].js",
-            // this got rid of the hash on style.css
-            assetFileNames: "assets/[name].[ext]",
-        },
-      },
-      // Prevent vendor.css being created
-      cssCodeSplit: false,
-      // prevent some warnings
-      chunkSizeWarningLimit: 60000,
-    },
     resolve: {
       alias: {
           '@': fileURLToPath(new URL('./src', import.meta.url))
