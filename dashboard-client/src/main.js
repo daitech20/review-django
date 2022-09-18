89595b05cd25b795636312a2f48b4f034e3357bc
Phuc Nguyen <phucnguyen@besmartee.com>
2022-09-18 03:37:41 +0000

    Remove ant-icon

--- dashboard-client/src/main.js
+++ dashboard-client/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import AntdIcon from './components/icon/AntdIcon.vue'
 import { createPinia } from 'pinia'
 import fakeAuth from './services/review_app/_fake.service'
 
@@ -11,7 +10,7 @@ import router from './routes'
 const app = createApp(App)
 app.use(createPinia())
 app.use(Antd)
-app.component('a-icon', AntdIcon)
+//app.component('a-icon', AntdIcon)
 app.use(router)
 
 if (parseInt(import.meta.env.VITE_ENABLE_MOCKING) === 1) {
